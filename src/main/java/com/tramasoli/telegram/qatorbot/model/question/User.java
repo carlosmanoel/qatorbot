f3908b1b1042c08330971d538e1636ab017a5db2
tramasoli <fabio@tramasoli.com>
2017-04-28 00:48:56 -0300

    Changes User tablename (postgres in a pain)

--- src/main/java/com/tramasoli/telegram/qatorbot/model/question/User.java
+++ src/main/java/com/tramasoli/telegram/qatorbot/model/question/User.java
@@ -8,6 +8,7 @@ import java.util.List;
  * Created by fabio on 18/04/17.
  */
 @Entity
+@Table(name="ChatUser")
 public class User implements Serializable{
 
     private static final long serialVersionUID = 1L;
