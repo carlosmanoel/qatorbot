c5e2f18facebc0703d1980245c465fc14a216d30
tramasoli <fabio@tramasoli.com>
2017-04-29 11:00:12 -0300

    Y U NO GO

--- src/main/java/com/tramasoli/telegram/qatorbot/telegram/commands/QuestionCommand.java
+++ src/main/java/com/tramasoli/telegram/qatorbot/telegram/commands/QuestionCommand.java
@@ -41,7 +41,7 @@ public class QuestionCommand extends BotCommand {
             question.setUser(asker);
             question.setText(String.join(" ", Arrays.asList(strings)));
             em.getTransaction().begin();
-            em.merge(chat);
+            em.merge(questionChat);
             em.getTransaction().commit();
             em.close();
             SendMessage message = new SendMessage();
